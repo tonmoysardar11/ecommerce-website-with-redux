8e0ad6cc88fb099b82db3009580de48ba7f1f71e
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-09-19 13:15:49 +0530

    tweak in prime section

--- src/components/pages/Prime.js
+++ src/components/pages/Prime.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 const Prime = () => {
   const [option, setoption] = useState(true);
   const [plan, setplan] = useState("prime");
-  
+
   const changeOption = () => {
     setoption(!option);
   };
   return (
     <section className="text-gray-600 body-font overflow-hidden">
-      <div className="container px-5 py-24 mx-auto">
+      <div className="container flex flex-col justify-center items-center px-5 py-24 mx-auto">
         <div className="flex flex-col justify-center items-center w-full mb-20">
           <span className="py-2 lg:py-0 inline-flex items-center bg-transparent border-0 py-1 px-3 focus:outline-none rounded text-3xl md:mt-0">
             Become <p className="text-4xl text-yellow-500 mx-1">Prime</p> Member
@@ -43,9 +43,13 @@ const Prime = () => {
             </button>
           </div>
         </div>
-        <div className="flex flex-wrap justify-center -m-4">
+        <div className="flex flex-wrap justify-center w-full -m-4">
           <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
-            <div className={`h-full p-6 rounded-lg border-2  ${plan==='basic'?'border-yellow-500':'border-gray-300'}  flex flex-col relative overflow-hidden`}>
+            <div
+              className={`h-full p-6 rounded-lg border-2  ${
+                plan === "basic" ? "border-yellow-500" : "border-gray-300"
+              }  flex flex-col relative overflow-hidden`}
+            >
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
                 Basic
               </h2>
@@ -53,7 +57,11 @@ const Prime = () => {
                 Free
               </h1>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='basic'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "basic" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -69,7 +77,11 @@ const Prime = () => {
                 Exclusive Items
               </p>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='basic'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "basic" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -85,7 +97,11 @@ const Prime = () => {
                 Premium Quality
               </p>
               <p className="flex items-center text-gray-600 mb-6">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='basic'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "basic" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -100,7 +116,12 @@ const Prime = () => {
                 </span>
                 Maximum Discounts
               </p>
-              <button className={`flex items-center mt-auto text-white ${plan==='basic'?'bg-yellow-500':'bg-gray-400'} border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded`} onClick={()=>setplan('basic')}>
+              <button
+                className={`flex items-center mt-auto text-white ${
+                  plan === "basic" ? "bg-yellow-500" : "bg-gray-400"
+                } border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded`}
+                onClick={() => setplan("basic")}
+              >
                 Select
                 <svg
                   fill="none"
@@ -117,8 +138,16 @@ const Prime = () => {
             </div>
           </div>
           <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
-            <div className={`h-full p-6 rounded-lg border-2 ${plan==='prime'?'border-yellow-500':'border-gray-400'} flex flex-col relative overflow-hidden`}>
-              <span className={`${plan==='prime'?'bg-yellow-500':'bg-gray-400'} text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl`}>
+            <div
+              className={`h-full p-6 rounded-lg border-2 ${
+                plan === "prime" ? "border-yellow-500" : "border-gray-400"
+              } flex flex-col relative overflow-hidden`}
+            >
+              <span
+                className={`${
+                  plan === "prime" ? "bg-yellow-500" : "bg-gray-400"
+                } text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl`}
+              >
                 POPULAR
               </span>
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
@@ -131,7 +160,11 @@ const Prime = () => {
                 </span>
               </h1>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -147,7 +180,11 @@ const Prime = () => {
                 Exclusive Items
               </p>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -163,7 +200,11 @@ const Prime = () => {
                 Premium Quality
               </p>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -179,7 +220,11 @@ const Prime = () => {
                 1-Day Delivery
               </p>
               <p className="flex items-center text-gray-600 mb-6">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -194,7 +239,12 @@ const Prime = () => {
                 </span>
                 Maximum Discounts
               </p>
-              <button className={`flex items-center mt-auto text-white ${plan==='prime'?'bg-yellow-500':'bg-gray-400'} border-0 py-2 px-4 w-full focus:outline-none hover:bg-yellow-600 rounded`} onClick={()=>setplan('prime')}>
+              <button
+                className={`flex items-center mt-auto text-white ${
+                  plan === "prime" ? "bg-yellow-500" : "bg-gray-400"
+                } border-0 py-2 px-4 w-full focus:outline-none hover:bg-yellow-600 rounded`}
+                onClick={() => setplan("prime")}
+              >
                 Select
                 <svg
                   fill="none"
@@ -211,8 +261,16 @@ const Prime = () => {
             </div>
           </div>
           <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
-            <div className={`h-full p-6 rounded-lg border-2 ${plan==='prime plus'?'border-yellow-500':'border-gray-400'} flex flex-col relative overflow-hidden`}>
-            <span className={`${plan==='prime plus'?'bg-yellow-500':'bg-gray-400'} text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl`}>
+            <div
+              className={`h-full p-6 rounded-lg border-2 ${
+                plan === "prime plus" ? "border-yellow-500" : "border-gray-400"
+              } flex flex-col relative overflow-hidden`}
+            >
+              <span
+                className={`${
+                  plan === "prime plus" ? "bg-yellow-500" : "bg-gray-400"
+                } text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl`}
+              >
                 BEST CHOICE
               </span>
               <h2 className="text-sm tracking-widest title-font mb-1 font-medium">
@@ -225,7 +283,11 @@ const Prime = () => {
                 </span>
               </h1>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime plus'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime plus" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -241,7 +303,11 @@ const Prime = () => {
                 Exclusive Items
               </p>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime plus'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime plus" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -257,7 +323,11 @@ const Prime = () => {
                 Premium Quality
               </p>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime plus'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime plus" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -273,7 +343,11 @@ const Prime = () => {
                 1-Day Delivery
               </p>
               <p className="flex items-center text-gray-600 mb-2">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime plus'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime plus" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -289,7 +363,11 @@ const Prime = () => {
                 24 X 7 Customer Support
               </p>
               <p className="flex items-center text-gray-600 mb-6">
-                <span className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${plan==='prime plus'?'bg-emerald-400':'bg-gray-400'} text-white rounded-full flex-shrink-0`}>
+                <span
+                  className={`w-4 h-4 mr-2 inline-flex items-center justify-center ${
+                    plan === "prime plus" ? "bg-emerald-400" : "bg-gray-400"
+                  } text-white rounded-full flex-shrink-0`}
+                >
                   <svg
                     fill="none"
                     stroke="currentColor"
@@ -304,7 +382,12 @@ const Prime = () => {
                 </span>
                 Maximum Discounts
               </p>
-              <button className={`flex items-center mt-auto text-white ${plan==='prime plus'?'bg-yellow-500':'bg-gray-400'} border-0 py-2 px-4 w-full focus:outline-none hover:bg-yellow-600 rounded`} onClick={()=>setplan('prime plus')}>
+              <button
+                className={`flex justify-center items-center mt-auto text-white ${
+                  plan === "prime plus" ? "bg-yellow-500" : "bg-gray-400"
+                } border-0 py-2 px-4 w-full focus:outline-none hover:bg-yellow-600 rounded`}
+                onClick={() => setplan("prime plus")}
+              >
                 Select
                 <svg
                   fill="none"
@@ -321,6 +404,7 @@ const Prime = () => {
             </div>
           </div>
         </div>
+       
       </div>
     </section>
   );
